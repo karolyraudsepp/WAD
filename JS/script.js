fa15321202f7ae61e3a3972c6b5a30d9a0ac10d7
KatiSoer <soerkati@gmail.com>
2021-10-20 22:20:37 +0200

    Wrote jquery to fetch data

--- JS/script.js
+++ JS/script.js
@@ -16,3 +16,33 @@ function myFunction() {
       }
     }
   }
+
+   $(function() {
+     //$.get("https://api.npoint.io/3427eb5cc1f62b180ff2", function(posts) {
+      $.get("JSON/posts.json", function(posts) {
+
+        for (post of posts) {
+
+            let div = $('<div class= "name">');
+            let title = $('<h1 class= "mdea">').text(post.name);
+            let body = $('<p>').text(post.body);
+
+            div.append('<img src="'+post.picture+'" style="width: 100%;" />');
+            div.append(title);
+            div.append(body);
+
+            let span = $('<span>')
+            span.append('<img src="Pictures/dislike.png" alt="Dislike icon" style="width: 10%;" />')
+            div.append('<img src="Pictures/like.png" alt="Like icon" style="width: 10%;" />')
+            div.append(span)
+
+            let container = $('<div class = "container2">');
+            container.append(div);
+            
+            let profile = $('<div class = "profile">');
+            profile.append(container);
+            
+            $(".for_background").append(profile);
+            }
+        })
+    });
